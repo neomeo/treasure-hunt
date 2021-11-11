89ab7c9aff8b7dd02bb9f6a8a5f24626214dcdf7
NeoMeo <35128627+neomeo@users.noreply.github.com>
2021-11-11 15:35:50 +0600

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export const config = {
   invite: '', // Invite Code is part of the Referral link: https://app.treasure.cloud/auth/signup?code=[INVITE CODE]
-  password: 'SvnKg7zFwkLc9mh9PAAWt',
+  password: 'MzJkNzQ3NGQtNDJkMS0xMWVjLWJjMTctNmIwNWFjMWQ2NDExOjg4M2Q2NDY1LTQyMzQtMTFlYy1iMzQ2LTI1Yjg2YTAyMzk3Nw==',
   screenshotsDir: './screenshots',
   runs: 120,
 };
